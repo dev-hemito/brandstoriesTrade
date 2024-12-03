2f017c216884d58a6262710483b1cb5a14cae4f1
yazin123 <mohammedyazin4@gmail.com>
2024-12-03 17:35:22 +0530

    demo payment setup for shiv khera, reversed

--- src/components/ShivKheraPlans.js
+++ src/components/ShivKheraPlans.js
@@ -7,7 +7,7 @@ const TicketPlans = () => {
     {
       id: 'silver',
       name: 'Silver Tickets',
-      price: 1,
+      price: 7999,
       features: ['Feature 1'],
       bgcolor: 'bg-gradient-to-br from-gray-50 to-gray-100',
     },
